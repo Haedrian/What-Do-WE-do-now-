0bdfdbc7419d194ac17f514d619a2816d4e492e2
Adrian <aadrianabela@gmail.com>
2015-01-23 23:36:24 +0100

    Scene loading code added

---
+++ Code/Assets/LoadAtRandom.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadAtRandom : MonoBehaviour {
+
+	/// <summary>
+	/// The integers of scene numbers containing game scenes
+	/// </summary>
+	public int[] SceneNumbers;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		int sceneRandom = SceneNumbers [Random.Range(0,(SceneNumbers.Length))];
+
+		Application.LoadLevel (sceneRandom);
+	}
+}
