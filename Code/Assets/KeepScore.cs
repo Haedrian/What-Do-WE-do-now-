549b498413541ac27d16705979b14cefbd167497
Adrian <aadrianabela@gmail.com>
2015-01-24 00:24:36 +0100

    Added code for game over and win

---
+++ Code/Assets/KeepScore.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepScore : MonoBehaviour {
+
+	public int Score = -1;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Score++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Code/Assets/LoadAtRandom.cs
+++ Code/Assets/LoadAtRandom.cs
@@ -1,23 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LoadAtRandom : MonoBehaviour {
 
 	/// <summary>
 	/// The integers of scene numbers containing game scenes
 	/// </summary>
-	public int[] SceneNumbers;
+	public List<int> SceneNumbers;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		DontDestroyOnLoad (gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		int sceneRandom = SceneNumbers [Random.Range(0,(SceneNumbers.Length))];
+		if (SceneNumbers.Count  <= 0)
+		{
+			//Player won!
+		}
+		else 
+		{
+			int sceneRandom = SceneNumbers [Random.Range(0,(SceneNumbers.Count))];
 
-		Application.LoadLevel (sceneRandom);
+			//Remove the scene number from the list
+			SceneNumbers.Remove (sceneRandom);
+
+			Application.LoadLevel (sceneRandom);
+		}
 	}
 }
