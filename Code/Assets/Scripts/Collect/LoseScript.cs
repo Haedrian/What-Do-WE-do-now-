5dce56ee11073b22e332db6c8be58b9129aa47c1
Adrian <aadrianabela@gmail.com>
2015-01-24 09:34:59 +0100

    Added Collect

---
+++ Code/Assets/Scripts/Collect/LoseScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseScript : MonoBehaviour {
+
+    public TimerRun controller;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //You lose!
+        controller.MissionComplete = false;
+    }
+}
