f7eb31ccda5af5d39834b1b1ada0815784eaf6b6
Adrian <aadrianabela@gmail.com>
2015-01-24 09:21:53 +0100

    Added Dodge

---
+++ Code/Assets/Scripts/Dodge/Movement.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(0, 0);
+    public TimerRun controller;
+
+    private Vector2 movement;
+
+    public static int ACC = 2;
+    public static int MAXSPEED = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            speed.x += -2;
+
+        }
+        else if (speed.x < 2)
+        {
+            speed.x = 0;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            speed.x += 2;
+        }
+        else if (speed.x > 2)
+        {
+            speed.x = 0;
+        }
+
+        if (speed.x > MAXSPEED)
+        {
+            speed.x = MAXSPEED;
+        }
+        else if (speed.x < -MAXSPEED)
+        {
+            speed.x = -MAXSPEED;
+        }
+
+        movement = new Vector2(speed.x, speed.y);
+
+        // 6 - Make sure we are not outside the camera bounds
+        var dist = (transform.position - Camera.main.transform.position).z;
+
+        var leftBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).x;
+
+        var rightBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(1, 0, dist)
+        ).x;
+
+        var topBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).y;
+
+        var bottomBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 1, dist)
+        ).y;
+
+        transform.position = new Vector3(
+          Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+          Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+          transform.position.z
+        );
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //You lose!
+        controller.MissionComplete = false;
+    }
+
+    void FixedUpdate()
+    {
+        // 5 - Move the game object
+        rigidbody2D.velocity = movement;
+    }
+}
---
+++ Code/Assets/Scripts/Dodge/ObstacleCreation.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleCreation : MonoBehaviour {
+
+    /// <summary>
+    /// The Obstacle to Create
+    /// </summary>
+    public Transform obstaclePrefab;
+
+    public TimerRun controller;
+
+    /// <summary>
+    /// How often to spawn
+    /// </summary>
+    public float spawnRate = 0.25f;
+
+    private float currentSpawnRate;
+
+    private float timer = 0f;
+
+    private float gameTimer = 0;
+
+    public float rotationMax = 0;
+
+    /// <summary>
+    /// Whether to generate this on the rightmost edge
+    /// </summary>
+    public bool maxOnXAxis = false;
+    /// <summary>
+    /// Whether to generate this on the topmost edge
+    /// </summary>
+    public bool maxOnYAxis = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentSpawnRate = spawnRate;
+        timer = spawnRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.InstructionsTimeLeft > 0)
+        {
+            //Do nothing
+            return;
+        }
+
+        gameTimer += Time.deltaTime;
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0)
+        {
+            //Spawn something
+            timer = Random.Range(currentSpawnRate * 0.75f, currentSpawnRate * 1.25f);
+
+            //Create a new obstacle
+            var obstacle = Instantiate(obstaclePrefab) as Transform;
+
+            //Assign it a random position, then force it to one of the sides if necessary
+            Vector2 randomPoint = new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+            if (maxOnXAxis)
+            {
+                randomPoint.x = 1f;
+            }
+
+            if (maxOnYAxis)
+            {
+                randomPoint.y = 1f;
+            }
+
+            var position = Camera.main.ViewportToWorldPoint(randomPoint);
+
+            position.z = 1;
+
+            obstacle.position = position;
+
+            Rigidbody2D rigid = obstacle.GetComponent<Rigidbody2D>();
+
+            //rigid.gravityScale = Random.Range(0, 2);
+            rigid.angularVelocity = Random.Range(0, rotationMax) * 15;
+        }
+
+        //Update current spawn rate
+        currentSpawnRate = spawnRate - (gameTimer / 35);
+
+        if (currentSpawnRate < 0.05)
+        {
+            currentSpawnRate = 0.05f;
+        }
+    }
+}
