63ae9faf9e37cbaae1e82a6842e06ad9e589c5ca
Adrian <aadrianabela@gmail.com>
2015-01-24 07:26:01 +0100

    Added Yellow 1

---
+++ Code/Assets/Scripts/Yellow/YellowKeyboardController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YellowKeyboardController : MonoBehaviour {
+
+	public TimerRun Timer;
+	public KeyCode E = KeyCode.E;
+	public KeyCode W = KeyCode.W;
+	
+	public Transform A;
+	public Transform F;
+
+	public KeyCode WinKey = KeyCode.E;
+
+	private bool ButtonPressed;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (GetComponent<TimerRun>().InstructionsTimeLeft > 0)
+		{
+			return;
+		}
+		
+		if (ButtonPressed) 
+		{
+			return;
+		}
+
+		if (Input.GetKeyDown(W))
+		{
+			ButtonPressed = true;
+			
+			if (W == WinKey)
+			{
+				this.GetComponent<TimerRun>().MissionComplete = true;
+				A.gameObject.SetActive(true);
+			}
+			else 
+			{
+                F.gameObject.SetActive(true);
+			}
+		}
+		else if (Input.GetKeyDown(E))
+		{
+			ButtonPressed = true;
+			
+			if (E == WinKey)
+			{
+				this.GetComponent<TimerRun>().MissionComplete = true;
+                A.gameObject.SetActive(true);
+			}
+			else 
+			{
+                F.gameObject.SetActive(true);
+			}
+
+		}
+		
+	}
+}
