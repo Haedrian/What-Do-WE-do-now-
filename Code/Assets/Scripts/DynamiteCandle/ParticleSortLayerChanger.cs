fff46313ebdb812aa89f4bf1c4463d3d56a22d4d
TrapperHell <trapper-hell@hotmail.com>
2015-01-24 02:59:42 +0100

    DynamiteCandle Scene: Added initial objects
    In addition, Scorpion scripts have been moved to Scripts directory.

---
+++ Code/Assets/Scripts/DynamiteCandle/ParticleSortLayerChanger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ParticleSortLayerChanger : MonoBehaviour
+{
+    void Start()
+    {
+        this.renderer.sortingLayerName = "Foreground";
+        this.renderer.sortingOrder = 2;
+    }
+}
