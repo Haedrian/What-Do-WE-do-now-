980de4c0961ed065943ddbc650afd2affce5ed45
TrapperHell <trapper-hell@hotmail.com>
2015-01-25 02:28:41 +0100

    Zombie Scene: Adjusted Object Selector

--- Code/Assets/Scripts/Zombie/ZObjectSelector.cs
+++ Code/Assets/Scripts/Zombie/ZObjectSelector.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class ZObjectSelector : MonoBehaviour {
+public class ZObjectSelector : MonoBehaviour
+{
+    public bool IsZombieScene;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        IsZombieScene = System.Convert.ToBoolean(Random.Range(0, 2));
+        Debug.Log("Is Zombie Scene? " + IsZombieScene);
+
+        GameObject ladder = GameObject.Find("Ladder");
+
+        if (this.IsZombieScene)
+        {
+        }
+        else
+        {
+            LadderController ladderController = ladder.GetComponent<LadderController>();
+
+            if (ladderController == null)
+                throw new MissingComponentException();
+
+            ladderController.enabled = true;
+        }
+    }
 }
